bd29e2454337ec6f72d25b7fdecc6ca1afa6f448
nombiezinja <tianyizhang1987@gmail.com>
2018-06-27 10:41:40 -0700

    adds working code for create table and insert, hard coded values

--- main.go
+++ main.go
@@ -12,17 +12,30 @@ import (
 func main() {
 	Setenv()
 
-	var DB *sql.DB
+	var db *sql.DB
 	var err error
 
 	dbstring := fmt.Sprintf("Driver=Apache Ignite;ADDRESS=%s;Cache=%s",
 		os.Getenv("address"), os.Getenv("cache"))
 
-	DB, err = sql.Open("odbc", dbstring)
+	db, err = sql.Open("odbc", dbstring)
 	u.FailOnError(err, "Failed to open ODBC connection")
 
-	err = DB.Ping()
+	err = db.Ping()
 	u.FailOnError(err, "Failed to ping ODBC")
 
 	fmt.Println("Apache Ignite connection successfully established")
+
+	created, err := db.Exec("CREATE TABLE TESTTEST(ID INT PRIMARY KEY, NAME VARCHAR(255));")
+	u.FailOnError(err, "Failed to create table")
+
+	fmt.Println(created)
+
+	inserted, err := db.Exec("INSERT INTO TESTTEST VALUES(17, 'BLEH')")
+	u.FailOnError(err, "Failed to insert")
+
+	fmt.Println(inserted)
+
+	// db.Exec("INSERT INTO Organization.Organization (name) VALUES ('Hp');")
+	// db.Exec("INSERT INTO Buys.BUY (ID, BaseAmount, UnfilledAmount, Price, UserID, CreatedAt) VALUES (1, 29, 29, 236, '2d931510-d99f-494a-8c67-87feb05e1594', 15003929329);")
 }
